f5ed7d48afb0772258f6d8c14912e29f655af8a7
Igor Clemente <igorelectrofest2011@>
2019-09-25 15:48:33 -0300

    BOOTCAMP_GOSTACK_ROCKETSHOES_REDUCERINTEGRATION_CREATED_25092019

--- rocketshoes/src/store/modules/cart/reducer.js
+++ rocketshoes/src/store/modules/cart/reducer.js
@@ -4,18 +4,7 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return producer(state, draf => {
-        const productIndex = state.findIndex(
-          product => product.id === action.product.id
-        );
-
-        if (productIndex >= 0) {
-          draf[productIndex].amount += 1;
-        } else {
-          draf.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draf.push(action.product);
       });
     case '@cart/REMOVE':
       return producer(state, draft => {
